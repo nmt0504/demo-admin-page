c16a5d119cd7fe6c27dd1d04f59bf12bffb40a61
Toan Nguyen <toannm3@vng.com.vn>
2018-05-02 14:22:56 +0700

    Init

--- src/dataProvider/rest.js
+++
@@ -1,7 +0,0 @@
-import simpleRestProvider from 'ra-data-simple-rest';
-
-const restDataProvider = simpleRestProvider('http://localhost:4000');
-export default (type, resource, params) =>
-    new Promise(resolve =>
-        setTimeout(() => resolve(restDataProvider(type, resource, params)), 500)
-    );
